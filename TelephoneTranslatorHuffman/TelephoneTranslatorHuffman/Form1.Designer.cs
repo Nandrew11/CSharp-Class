6365a423b99e8cca32ec2e104612d5cab5f3f5fa
Nandrew11 <168987564+Nandrew11@users.noreply.github.com>
2024-05-05 14:29:10 -0400

    Telephone Translator

---
+++ TelephoneTranslatorHuffman/TelephoneTranslatorHuffman/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TelephoneTranslatorHuffman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertPhoneNumberButton = new System.Windows.Forms.Button();
+            this.PhoneNumberTextbox = new System.Windows.Forms.TextBox();
+            this.ConvertedNumberLabel = new System.Windows.Forms.Label();
+            this.EnterNumberLabel = new System.Windows.Forms.Label();
+            this.ClearScreenButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConvertPhoneNumberButton
+            // 
+            this.ConvertPhoneNumberButton.Location = new System.Drawing.Point(93, 153);
+            this.ConvertPhoneNumberButton.Name = "ConvertPhoneNumberButton";
+            this.ConvertPhoneNumberButton.Size = new System.Drawing.Size(129, 40);
+            this.ConvertPhoneNumberButton.TabIndex = 6;
+            this.ConvertPhoneNumberButton.Text = "Display Numeric Phone Number";
+            this.ConvertPhoneNumberButton.UseVisualStyleBackColor = true;
+            this.ConvertPhoneNumberButton.Click += new System.EventHandler(this.ConvertPhoneNumberButton_Click);
+            // 
+            // PhoneNumberTextbox
+            // 
+            this.PhoneNumberTextbox.Location = new System.Drawing.Point(93, 108);
+            this.PhoneNumberTextbox.Name = "PhoneNumberTextbox";
+            this.PhoneNumberTextbox.Size = new System.Drawing.Size(129, 20);
+            this.PhoneNumberTextbox.TabIndex = 7;
+            // 
+            // ConvertedNumberLabel
+            // 
+            this.ConvertedNumberLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ConvertedNumberLabel.Location = new System.Drawing.Point(93, 223);
+            this.ConvertedNumberLabel.Name = "ConvertedNumberLabel";
+            this.ConvertedNumberLabel.Size = new System.Drawing.Size(129, 23);
+            this.ConvertedNumberLabel.TabIndex = 8;
+            // 
+            // EnterNumberLabel
+            // 
+            this.EnterNumberLabel.Location = new System.Drawing.Point(120, 70);
+            this.EnterNumberLabel.Name = "EnterNumberLabel";
+            this.EnterNumberLabel.Size = new System.Drawing.Size(84, 35);
+            this.EnterNumberLabel.TabIndex = 9;
+            this.EnterNumberLabel.Text = "Enter Phone Number Below:";
+            // 
+            // ClearScreenButton
+            // 
+            this.ClearScreenButton.Location = new System.Drawing.Point(93, 270);
+            this.ClearScreenButton.Name = "ClearScreenButton";
+            this.ClearScreenButton.Size = new System.Drawing.Size(129, 34);
+            this.ClearScreenButton.TabIndex = 10;
+            this.ClearScreenButton.Text = "Clear screen for new entry.";
+            this.ClearScreenButton.UseVisualStyleBackColor = true;
+            this.ClearScreenButton.Click += new System.EventHandler(this.ClearScreenButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(123, 322);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 11;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 450);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearScreenButton);
+            this.Controls.Add(this.EnterNumberLabel);
+            this.Controls.Add(this.ConvertedNumberLabel);
+            this.Controls.Add(this.PhoneNumberTextbox);
+            this.Controls.Add(this.ConvertPhoneNumberButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertPhoneNumberButton;
+        private System.Windows.Forms.TextBox PhoneNumberTextbox;
+        private System.Windows.Forms.Label ConvertedNumberLabel;
+        private System.Windows.Forms.Label EnterNumberLabel;
+        private System.Windows.Forms.Button ClearScreenButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ TelephoneTranslatorHuffman/TelephoneTranslatorHuffman/Form1.cs
@@ -0,0 +1,154 @@
+﻿//Nathan Huffman
+//4/29/2023
+//this is desycering the numbers for the get food number 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneTranslatorHuffman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //closes program
+            Close();
+        }
+
+        private void ClearScreenButton_Click(object sender, EventArgs e)
+        {
+            //Clear the textbox as well as the label
+            PhoneNumberTextbox.Clear();
+            ConvertedNumberLabel.Text = "";
+        }
+
+        private void ConvertPhoneNumberButton_Click(object sender, EventArgs e)
+        {
+            //get and validate the numbers entered by user
+            string phonenumber = null;
+            string SemiConvertedNumber = null;
+            string ConvertedNumber = null;
+
+            if (IsValidFormat(ConvertedNumber))
+            {
+                SemiConvertedNumber = UndoPhoneNumber(phonenumber);
+
+                ConvertedNumber = CharConversion(SemiConvertedNumber);
+
+                ConvertedNumber = ConvertedNumber.Insert(3, "-");
+                ConvertedNumber = ConvertedNumber.Insert(7, "-");
+
+                ConvertedNumberLabel.Text = ConvertedNumber;
+
+            }
+            else
+            {
+                MessageBox.Show("Enter number in this way:" + "\n" + "XXX-XXX-XXXX");
+            }
+        }
+
+        private bool IsValidFormat(string ValidNumber)
+        {
+            //make sure the user number is in good format
+            const int VALID_LENGTH = 12;
+            ValidNumber = PhoneNumberTextbox.Text;
+            bool isNumValid;
+            // leading and trailing spaces already removed:
+            if (ValidNumber.Length == VALID_LENGTH && ValidNumber[3] == '-' && ValidNumber[7] == '-')
+            {
+                isNumValid = true;
+            }
+            else
+            {
+                isNumValid = false;
+            }
+
+            return isNumValid;
+        }
+
+
+        
+
+        private string CharConversion(string newNumber)
+        {
+            //check which characters are letters and convert to corresponding number
+            int x = 0;
+            string UnformattedNumber = UndoPhoneNumber(newNumber);
+            newNumber = null;
+
+            char unconvertednumberchar;
+            for (x = 0; x < 10; x++)
+            {
+                if (char.IsDigit(UnformattedNumber[x]))
+                {
+                    newNumber += UnformattedNumber[x];
+                }
+
+                if (char.IsLetter(UnformattedNumber[x]))
+                {
+                    unconvertednumberchar = UnformattedNumber[x];
+
+                    if (unconvertednumberchar == 'a' || unconvertednumberchar == 'A' || unconvertednumberchar == 'b' || unconvertednumberchar == 'B' || unconvertednumberchar == 'c' || unconvertednumberchar == 'C')
+                    {
+                        newNumber += 2.ToString();
+                    }
+                    else if (unconvertednumberchar == 'd' || unconvertednumberchar == 'D' || unconvertednumberchar == 'e' || unconvertednumberchar == 'E' || unconvertednumberchar == 'f' || unconvertednumberchar == 'F')
+                    {
+                        newNumber += 3.ToString();
+                    }
+                    else if (unconvertednumberchar == 'g' || unconvertednumberchar == 'G' || unconvertednumberchar == 'h' || unconvertednumberchar == 'H' || unconvertednumberchar == 'i' || unconvertednumberchar == 'I')
+                    {
+                        newNumber += 4.ToString();
+                    }
+                    else if (unconvertednumberchar == 'j' || unconvertednumberchar == 'J' || unconvertednumberchar == 'k' || unconvertednumberchar == 'K' || unconvertednumberchar == 'l' || unconvertednumberchar == 'L')
+                    {
+                        newNumber += 5.ToString();
+                    }
+                    else if (unconvertednumberchar == 'm' || unconvertednumberchar == 'M' || unconvertednumberchar == 'n' || unconvertednumberchar == 'N' || unconvertednumberchar == 'o' || unconvertednumberchar == 'O')
+                    {
+                        newNumber += 6.ToString();
+                    }
+                    else if (unconvertednumberchar == 'p' || unconvertednumberchar == 'P' || unconvertednumberchar == 'q' || unconvertednumberchar == 'Q' || unconvertednumberchar == 'r' || unconvertednumberchar == 'R' || unconvertednumberchar == 's' || unconvertednumberchar == 'S')
+                    {
+                        newNumber += 7.ToString();
+                    }
+                    else if (unconvertednumberchar == 't' || unconvertednumberchar == 'T' || unconvertednumberchar == 'u' || unconvertednumberchar == 'U' || unconvertednumberchar == 'v' || unconvertednumberchar == 'V')
+                    {
+                        newNumber += 8.ToString();
+                    }
+                    else if (unconvertednumberchar == 'w' || unconvertednumberchar == 'W' || unconvertednumberchar == 'x' || unconvertednumberchar == 'X' || unconvertednumberchar == 'y' || unconvertednumberchar == 'Y' || unconvertednumberchar == 'z' || unconvertednumberchar == 'Z')
+                    {
+                        newNumber += 9.ToString();
+                    }
+                }
+
+            }
+
+            return newNumber;
+
+        }
+
+        private string UndoPhoneNumber(string UnconvertedNumber)
+        {
+            //unformat number so the hyphens are gone temporarily
+            UnconvertedNumber = PhoneNumberTextbox.Text;
+
+            UnconvertedNumber = UnconvertedNumber.Remove(3, 1);
+            UnconvertedNumber = UnconvertedNumber.Remove(6, 1);
+
+            return UnconvertedNumber;
+        }
+    }
+}
